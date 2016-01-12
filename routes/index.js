08ef694ca0f88c624696c6f15f603c30a6803beb
sleuthg <sleuthg@gmail.com>
2016-01-12 10:02:30 -0800

    initial commit of main files for practice

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET chat sample */
+router.get('/chat', function(req, res) {
+  res.render('chat', {});
+});
+
+module.exports = router;
